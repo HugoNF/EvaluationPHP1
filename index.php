b464a5dbbf0425b542f3e727cdd92058e08531ad
HugoNF <32874545+HugoNF@users.noreply.github.com>
2018-02-09 14:14:13 +0100

    Première Partie**

--- index.php
+++ index.php
@@ -10,11 +10,11 @@ $Telephone = array ('0700000000');
 $date = array ('1998-07-22');
 
 
-for ($numero = 0; $numero < 1; $numero++)
+for ($tableau = 0; $tableau < 1; $tableau++)
 {
-    echo  '<ul>' . '<li>' .'<b>'.'Prenom :'.'</b>' . ' ' . $Prenom[$numero] .'<li>'.'<b>'.'Nom :'.'</b>' . ' ' .
-        $Nom[$numero] .'<li>'.'<b>'.'Adresse :'.'</b>' . ' ' . $Adresse[$numero] .'<li>'.'<b>'.'Code Postale :'.'</b>' . ' ' .
-        $CodePostal[$numero] .'<li>'.'<b>'.'Ville :'.'</b>' . ' ' . $Ville[$numero] .'<li>'.'<b>'.'Téléphone :'.'</b>' . ' ' .
-        $Telephone[$numero] .'<li>'.'<b>'.'Email :'.'</b>'.' '.$Email[$numero].'<li>'.'<b>'.'Date de Naissance :'.'</b>' . ' ' . $date[$numero] .'<ul/>';
+    echo  '<ul>' . '<li>' .'<b>'.'Prenom :'.'</b>' . ' ' . $Prenom[$tableau] .'<li>'.'<b>'.'Nom :'.'</b>' . ' ' .
+        $Nom[$tableau] .'<li>'.'<b>'.'Adresse :'.'</b>' . ' ' . $Adresse[$tableau] .'<li>'.'<b>'.'Code Postale :'.'</b>' . ' ' .
+        $CodePostal[$tableau] .'<li>'.'<b>'.'Ville :'.'</b>' . ' ' . $Ville[$tableau] .'<li>'.'<b>'.'Téléphone :'.'</b>' . ' ' .
+        $Telephone[$tableau] .'<li>'.'<b>'.'Email :'.'</b>'.' '.$Email[$tableau].'<li>'.'<b>'.'Date de Naissance :'.'</b>' . ' ' . $date[$tableau] .'<ul/>';
 }
 ?>
